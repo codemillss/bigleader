69f090ed5ee46b89b1cdabd2f2794ea4a4da9942
codemillss <ggzgcode10000@gmail.com>
2023-07-02 17:21:53 +0900

    first commit

---
+++ Algorithmn/main.py
@@ -0,0 +1,60 @@
+import sys
+
+'''
+result_int = int(sys.stdin.readline())
+
+print(type(result_int), (result_int))
+print(type(int(result_int)), int((result_int)))
+
+
+
+a,b,c = map(int, sys.stdin.readline().split())
+print(a,b,c)
+
+res_list = list(map(int, sys.stdin.readline().split()))
+print(res_list)
+
+
+list_in_list = list()
+n= int(input())
+for i in range(n):
+    list_in_list.append(list(map(int,sys.stdin.readline().split())))
+print(list_in_list)
+
+
+print("hi")
+print()
+print("hello","my","name","is")
+print("hi",end="")
+print("hello","my","name","is", sep="")
+print("hello","my","name","is", sep="\n")
+'''
+
+
+'''
+ipt = int(input())
+for i in range(ipt) :
+    print(" "*i+"*"*(ipt-i))
+
+ipt = int(input())
+for i in range(1,ipt+1) :
+    print(" "*(ipt-i)+"*"*i)
+
+
+N = int(input())
+for i in range(N) :
+    print(i*" "+((2*(N-i-1)+1))*"*")
+for i in range(1,N) :
+    print(f"{(N-i-1)*' '}{(2*i+1)*'*'}")
+ 
+N = int(input())
+for i in range(1,N) :
+    print()
+'''
+import sys
+N,M,x,y,K = map(int,sys.stdin.readline().split())
+a=[]
+for i in range(N) :
+    a.append(list(map(int,sys.stdin.readline().split())))
+list(map(int,sys.stdin.readline().split()))
+
